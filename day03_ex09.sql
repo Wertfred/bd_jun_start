0e972940062ed1180f5a4f41e5768d25ec1c0ef5
Wertfred <vaceslavludin@gmail.com>
2025-06-24 15:36:23 +0300

    Update day03_ex09.sql

--- day03_ex09.sql
+++ day03_ex09.sql
@@ -3,7 +3,7 @@ values ((select max(id) + 1 from person_visits),
         (select id from person where name = 'Denis'),
         (select id from pizzeria where name = 'Dominos'),
         '2022-02-24'),
-        ((select MAX(id) + 2 from person_visits),
+        ((select max(id) + 2 from person_visits),
          (select id from person where name = 'Irina'),
          (select id from pizzeria where name = 'Dominos'),
          '2022-02-24');

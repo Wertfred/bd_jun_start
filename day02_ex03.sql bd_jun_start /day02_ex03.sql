e1e27e854e0202ba8653c8ffbb8b6d78dfd2ca0d
Wertfred <vaceslavludin@gmail.com>
2025-06-24 14:43:10 +0300

    Delete day02_ex03.sql bd_jun_start /day02_ex03.sql

--- day02_ex03.sql bd_jun_start /day02_ex03.sql
+++
@@ -1,4 +0,0 @@
-select pizza_name, name as pizzeria_name, price
-from menu join pizzeria on pizzeria_id = pizzeria.id
-where pizza_name = 'mushroom pizza' or pizza_name = 'pepperoni pizza'
-order by pizza_name, name;
